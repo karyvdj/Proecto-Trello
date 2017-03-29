cac292c6f6501d23ae9f3ac2f25be77a0a7aa26a
OfeliaGarc <ofee.50.og@gmail.com>
2017-03-29 11:03:00 -0600

    Crear pendiente funciona en lista ejemplo

--- assets/js/funciones.js
+++ assets/js/funciones.js
@@ -1,8 +1,28 @@
 //alert("hola");
 function crearLista() {
   var lista = document.getElementById('nombreLista').value;
+  //console.log(lista)
+  var nuevaLista = document.createElement("selection");
+  nuevaLista.innerHTML = lista;
+  document.getElementById('lista').appendChild(nuevaLista);
+
+  /*var lista = document.getElementById('nombreLista').value;
+  //console.log(lista)
   var publicarLista = document.createElement("p");
   var texto= document.createTextNode(lista);
   publicarLista.appendChild(texto);
-  //publicarLista.innerHTML= lista;
+  //publicarLista.innerHTML= lista*/
+}
+
+
+//Funciona en la lista de ejemplo//
+function crearPendiente(){
+  var pendiente = document.getElementById('agregarPendiente').value;
+  if(pendiente != ""){
+    var nuevoPendiente = document.createElement("li");
+    nuevoPendiente.innerHTML = pendiente;
+    document.getElementById("listaOrdenada").appendChild(nuevoPendiente);
+    //borrar casilla//
+    document.getElementById('agregarPendiente').value = "";
+  }
 }
