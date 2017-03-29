455e9214f0034c51e5334974cb5ea92f3f8d19c2
OfeliaGarc <ofee.50.og@gmail.com>
2017-03-29 10:27:39 -0600

    Crear lista aun en proceso

--- assets/js/funciones.js
+++ assets/js/funciones.js
@@ -1,1 +1,8 @@
-alert("hola");
+//alert("hola");
+function crearLista() {
+  var lista = document.getElementById('nombreLista').value;
+  var publicarLista = document.createElement("p");
+  var texto= document.createTextNode(lista);
+  publicarLista.appendChild(texto);
+  //publicarLista.innerHTML= lista;
+}
